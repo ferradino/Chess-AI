c61cdfdf346f8aec790137ae78767f5b600639d7
ferradino <nicholasferradino@icloud.com>
2023-10-03 18:32:22 -0400

    add constants file

---
+++ const.py
@@ -0,0 +1,10 @@
+# Need width and height for the window
+# As well as the number of squres and their sizes
+WIDTH = 800
+HEIGHT = 800
+
+# Need to define colors as variables
+BLACK = (0, 0, 0)
+
+# Frame Rate
+FPS = 60
