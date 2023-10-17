dd0c31ae9dead303f0cfe31ace96e87d28e53249
Matthew Belcik <mattbelcikwork@gmail.com>
2023-10-17 19:51:57 -0400

    started en passant and castling

--- board.py
+++ board.py
@@ -1,7 +1,8 @@
 from const import *
-from pieces import * 
+from pieces import *
 
 import pygame
+
 
 class Board:
     def __init__(self):
@@ -11,7 +12,7 @@ class Board:
 
         # Text Font
         self.font = pygame.font.Font(pygame.font.get_default_font(), 20)
-        
+
         # Initial setup of the chessboard with pieces in their starting positions.
         # Game board stored as an array of piece objects
         self.board = [
@@ -27,20 +28,16 @@ class Board:
                 Knight("black"),
                 Rook("black"),
             ],
-
             # Row 7
             [Pawn("black") for _ in range(8)],
-
             # Rows 6 - 3
             # Colums A-H
             [None for _ in range(8)],
             [None for _ in range(8)],
             [None for _ in range(8)],
             [None for _ in range(8)],
-
             # Row 2
             [Pawn("white") for _ in range(8)],
-
             # Row 1
             [
                 # Columns A-H
@@ -54,20 +51,36 @@ class Board:
                 Rook("white"),
             ],
         ]
+        self.last_move = None
+
+    def is_in_check(self, color):
+        king_position = self.get_king_position(color)
+        for y, row in enumerate(self.board):
+            for x, piece in enumerate(row):
+                if piece and piece.color != color:
+                    if king_position in piece.possible_moves((x, y), self):
+                        return True
+        return False
+
+    def get_king_position(self, color):
+        for y, row in enumerate(self.board):
+            for x, piece in enumerate(row):
+                if isinstance(piece, King) and piece.color == color:
+                    return (x, y)
 
     # Draws the row numbers on the board
     # Gets correct color and then draws on board
     def drawRowNumbers(self):
         for r in range(ROWS):
-            color = LIGHT_SQAURE_COLOR if (r + 1) % 2 == 0 else DARK_SQUARE_COLOR 
-            text = self.font.render(str(ROWS - r), 1, color)    
+            color = LIGHT_SQAURE_COLOR if (r + 1) % 2 == 0 else DARK_SQUARE_COLOR
+            text = self.font.render(str(ROWS - r), 1, color)
             self.window.blit(text, (110, 110 + r * SQSIZE))
 
     # Draws the column letters on the board
     # Gets correct color and then draws on board
-    def drawColumnLetters(self): 
+    def drawColumnLetters(self):
         for c in range(COLS):
-            color = DARK_SQUARE_COLOR if (c + 1) % 2 == 0 else LIGHT_SQAURE_COLOR 
+            color = DARK_SQUARE_COLOR if (c + 1) % 2 == 0 else LIGHT_SQAURE_COLOR
             text = self.font.render(chr(c + 97), 1, color)
             self.window.blit(text, ((100 + SQSIZE - 20) + c * SQSIZE, HEIGHT - 125))
 
@@ -79,8 +92,8 @@ class Board:
                 piece = self.board[r][c]
                 if piece:
                     self.window.blit(piece.img, (100 + c * SQSIZE, 100 + r * SQSIZE))
-    
-    # Drawing board 
+
+    # Drawing board
     # Loops to get correct square color and then draws square
     def drawBoard(self):
         self.window.fill(GREY)
@@ -88,14 +101,14 @@ class Board:
             for c in range(COLS):
                 # Deciding correct sqaure color
                 color = LIGHT_SQAURE_COLOR if (r + c) % 2 == 0 else DARK_SQUARE_COLOR
-                pygame.draw.rect(self.window, color, (100 + c * SQSIZE, 100 + r * SQSIZE, SQSIZE, SQSIZE))
-            
+                pygame.draw.rect(
+                    self.window,
+                    color,
+                    (100 + c * SQSIZE, 100 + r * SQSIZE, SQSIZE, SQSIZE),
+                )
+
                 if r == ROWS - 1:
                     self.drawColumnLetters()
-        
+
         self.drawRowNumbers()
         self.drawPieces()
-   
-
-
- 
--- pieces.py
+++ pieces.py
@@ -7,7 +7,9 @@ import os
 class ChessPiece:
     def __init__(self, color, img):
         self.color = color  # Color of the piece (white or black).
-        self.img = pygame.transform.scale(pygame.image.load(os.path.join('images', img)), (SQSIZE, SQSIZE))  # Load piece image and scale to size
+        self.img = pygame.transform.scale(
+            pygame.image.load(os.path.join("images", img)), (SQSIZE, SQSIZE)
+        )  # Load piece image and scale to size
 
     # Placeholder method for possible moves.
     # Specific piece classes will override this with their movement logic.
@@ -21,7 +23,7 @@ class Pawn(ChessPiece):
         img = "wp.png" if color == "white" else "bp.png"
         super().__init__(color, img)
 
-    def possible_moves(self, position):
+    def possible_moves(self, position, board):
         x, y = position  # Extracting the row and column from the position.
         moves = []  # A list to store all possible moves.
         # If the pawn is white, it moves upwards (i.e., row increases).
@@ -37,6 +39,27 @@ class Pawn(ChessPiece):
                 moves.append((x, y - 1))
             if y == 6:
                 moves.append((x, y - 2))
+
+            # Pawn Promotion logic
+        if self.color == "white" and y == 0:
+            board.promote_pawn(x, y)
+        elif self.color == "black" and y == 7:
+            board.promote_pawn(x, y)
+
+            # En Passant
+        if board.last_move:
+            last_piece_moved, last_start, last_end = board.last_move
+            if (
+                isinstance(last_piece_moved, Pawn)
+                and abs(last_start[1] - last_end[1]) == 2
+            ):
+                if self.color == "white" and y == 3:
+                    if x - 1 == last_end[0] or x + 1 == last_end[0]:
+                        moves.append((last_end[0], last_end[1] - 1))
+                elif self.color == "black" and y == 4:
+                    if x - 1 == last_end[0] or x + 1 == last_end[0]:
+                        moves.append((last_end[0], last_end[1] + 1))
+
         return moves  # Return the list of valid moves.
 
 
@@ -44,9 +67,10 @@ class Pawn(ChessPiece):
 class Rook(ChessPiece):
     def __init__(self, color):
         self.img = "wR.png" if color == "white" else "bR.png"
+        self.has_moved = False
         super().__init__(color, self.img)
 
-    def possible_moves(self, position):
+    def possible_moves(self, position, board):
         x, y = position
         moves = []
         # Rook can move to any other position in its current row or column.
@@ -64,7 +88,7 @@ class Bishop(ChessPiece):
         img = "wB.png" if color == "white" else "bB.png"
         super().__init__(color, img)
 
-    def possible_moves(self, position):
+    def possible_moves(self, position, board):
         x, y = position
         moves = []
         # The bishop can move any number of squares diagonally.
@@ -93,7 +117,7 @@ class Knight(ChessPiece):
     # Defining all possible moves for a knight.
     MOVES = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1)]
 
-    def possible_moves(self, position):
+    def possible_moves(self, position, board):
         x, y = position
         moves = []
         # The knight can jump to any of the positions defined in MOVES.
@@ -110,7 +134,7 @@ class Queen(ChessPiece):
         img = "wQ.png" if color == "white" else "bQ.png"
         super().__init__(color, img)
 
-    def possible_moves(self, position):
+    def possible_moves(self, position, board):
         # The queen can move like both a rook and a bishop.
         return Rook(self.color).possible_moves(position) + Bishop(
             self.color
@@ -121,12 +145,13 @@ class Queen(ChessPiece):
 class King(ChessPiece):
     def __init__(self, color):
         img = "wK.png" if color == "white" else "bK.png"
+        self.has_moved = False
         super().__init__(color, img)
 
     # Defining all possible moves for a king.
     MOVES = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
-    def possible_moves(self, position):
+    def possible_moves(self, position, board):
         x, y = position
         moves = []
         # The king can move one square in any direction.
@@ -134,4 +159,21 @@ class King(ChessPiece):
             nx, ny = x + dx, y + dy
             if 0 <= nx < 8 and 0 <= ny < 8:
                 moves.append((nx, ny))
+        # Castling logic
+        if not self.has_moved and not board.is_in_check(self.color):
+            # Check for king-side castling (O-O)
+            if isinstance(board[y][7], Rook) and not board[y][7].has_moved:
+                if not board[y][5] and not board[y][6]:
+                    if not board.is_square_under_attack(
+                        5, y, self.color
+                    ) and not board.is_square_under_attack(6, y, self.color):
+                        moves.append((6, y))
+
+            # Check for queen-side castling (O-O-O)
+            if isinstance(board[y][0], Rook) and not board[y][0].has_moved:
+                if not board[y][1] and not board[y][2] and not board[y][3]:
+                    if not board.is_square_under_attack(
+                        3, y, self.color
+                    ) and not board.is_square_under_attack(2, y, self.color):
+                        moves.append((2, y))
         return moves
